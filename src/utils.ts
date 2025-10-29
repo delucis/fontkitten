ecc87fa8dd4117a02309347cfc0400d98a6dac67
Chris Swithinbank <swithinbank@gmail.com>
2025-10-29 13:25:16 +0100

    Clean up `src/utils.ts`

--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,28 @@
-export function binarySearch(arr, cmp) {
-  let min = 0;
-  let max = arr.length - 1;
-  while (min <= max) {
-    let mid = (min + max) >> 1;
-    let res = cmp(arr[mid]);
+export function binarySearch<T>(arr: T[], cmp: (item: T) => number): number {
+	let min = 0;
+	let max = arr.length - 1;
+	while (min <= max) {
+		const mid = (min + max) >> 1;
+		const res = cmp(arr[mid]);
 
-    if (res < 0) {
-      max = mid - 1;
-    } else if (res > 0) {
-      min = mid + 1;
-    } else {
-      return mid;
-    }
-  }
+		if (res < 0) {
+			max = mid - 1;
+		} else if (res > 0) {
+			min = mid + 1;
+		} else {
+			return mid;
+		}
+	}
 
-  return -1;
+	return -1;
 }
 
-export function range(index, end) {
-  let range = [];
-  while (index < end) {
-    range.push(index++);
-  }
-  return range;
+export function range(index: number, end: number): number[] {
+	const range: number[] = [];
+	while (index < end) {
+		range.push(index++);
+	}
+	return range;
 }
 
 export const asciiDecoder = new TextDecoder('ascii');
-
-// Based on https://github.com/niklasvh/base64-arraybuffer. MIT license.
-const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-const LOOKUP = new Uint8Array(256);
-for (let i = 0; i < CHARS.length; i++) {
-  LOOKUP[CHARS.charCodeAt(i)] = i;
-}

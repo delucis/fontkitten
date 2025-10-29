4a9d5b3d6717bef0a6d71ab8dce082b14cfb350b
Chris Swithinbank <swithinbank@gmail.com>
2025-10-29 13:29:28 +0100

    Start cleaning up `src/index.ts`

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,18 @@ import DFont from './DFont';
 
 const formats = [TTFFont, WOFFFont, WOFF2Font, TrueTypeCollection, DFont];
 
-export function create(buffer, postscriptName) {
-	for (let i = 0; i < formats.length; i++) {
-		let format = formats[i];
+/**
+ * Returns a font object for the given buffer.
+ * For collection fonts (such as TrueType collection files), you can pass a postscriptName to get
+ * that font out of the collection instead of a collection object.
+ * @param buffer `Buffer` containing font data
+ * @param postscriptName Optional PostScript name of font to extract from collection file.
+ */
+// TODO: Add proper return type
+export function create(buffer: Buffer, postscriptName?: string) {
+	for (const format of formats) {
 		if (format.probe(buffer)) {
-			let font = new format(new DecodeStream(buffer));
+			const font = new format(new DecodeStream(buffer));
 			if (postscriptName) {
 				return font.getFont(postscriptName);
 			}

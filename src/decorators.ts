477c9df235a7864c7446039400a42bd818875384
Chris Swithinbank <swithinbank@gmail.com>
2025-10-30 19:05:36 +0100

    Types for the `@cache` decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,36 +1,40 @@
 /**
  * This decorator caches the results of a getter or method such that
  * the results are lazily computed once, and then cached.
- * @private
  */
-export function cache(target, key, descriptor) {
+export function cache<T extends any>(
+  _target: any,
+  key: string,
+  descriptor: TypedPropertyDescriptor<T>
+): TypedPropertyDescriptor<T> {
   if (descriptor.get) {
-    let get = descriptor.get;
+    const get = descriptor.get;
     descriptor.get = function() {
-      let value = get.call(this);
+      const value = get.call(this);
       Object.defineProperty(this, key, { value });
       return value;
     };
   } else if (typeof descriptor.value === 'function') {
-    let fn = descriptor.value;
+    const fn = descriptor.value;
 
     return {
       get() {
-        let cache = new Map;
-        function memoized(...args) {
-          let key = args.length > 0 ? args[0] : 'value';
+        const cache = new Map();
+        const memoized = ((...args: any[]) => {
+          const key = args.length > 0 ? args[0] : 'value';
           if (cache.has(key)) {
             return cache.get(key);
           }
 
-          let result = fn.apply(this, args);
+          const result = fn.apply(this, args);
           cache.set(key, result);
           return result;
-        };
+        }) as T;
 
         Object.defineProperty(this, key, {value: memoized});
         return memoized;
       }
     };
   }
+  return descriptor;
 }
